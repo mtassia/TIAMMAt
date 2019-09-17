0cc20fe95af8220995a07e63523b25c058b3b22b
Michael G Tassia <mgt0007@auburn.edu>
2019-09-17 12:41:37 -0500

    Added script which will non-destructively rename fasta headers in the SECOND_SEARCH/ directories for each domain revision. Fasta headers will be appended with domain IDs for the target domain being revised and 'NOV' if the sequence was not found prior to revision.

---
+++ ID_novel_seqs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Execute in SECOND_SEARCH directory from IRDS
+
+for NOVEL_LIST in *.sequences_only_identified_after_revision.txt
+do
+	TAXON_ID=`echo "${NOVEL_LIST}" | awk -F"." '{print $1}'`
+	DOMAIN_ID=`echo "${NOVEL_LIST}" | awk -F"." '{print $2}'`
+	FASTAFILE_ID=`echo "${TAXON_ID}.${DOMAIN_ID}_base_and_revised_present.fasta"`
+	
+	if [[ -s ${NOVEL_LIST} ]]; then #Run two different loops depending on whether novel seqs were identfied
+		cp ${FASTAFILE_ID} ${TAXON_ID}.${DOMAIN_ID}_base_and_revised_present.original_names.fasta #Create a file of the original naming convention
+
+		sed "/>/s/>.*/&_${DOMAIN_ID}/" ${FASTAFILE_ID} > ${FASTAFILE_ID}.renaming_tmp #Sed to add domain IDs to the end of each fasta header and make a new file for renamed seqs
+
+		while read NOVEL_ACCESSION #Read the newly identified seq list
+		do
+			sed -i "/>/s/${NOVEL_ACCESSION}.*/&_NOV/" ${FASTAFILE_ID}.renaming_tmp #Append "NOV" to headers of sequences which weren't identfied before domain revision
+		done < ${NOVEL_LIST}
+		mv ${FASTAFILE_ID}.renaming_tmp ${TAXON_ID}.${DOMAIN_ID}_base_and_revised_present.IDs_renamed.fasta #Rename the temporary renaming file
+	else
+		cp ${FASTAFILE_ID} ${TAXON_ID}.${DOMAIN_ID}_base_and_revised_present.original_names.fasta #Create a file of the original naming convention
+		sed "/>/s/>.*/&_${DOMAIN_ID}/" ${FASTAFILE_ID} > ${FASTAFILE_ID}.IDs_renamed.fasta #For fasta files without newly identfied sequences, append the domain ID to header.
+	fi
+done
