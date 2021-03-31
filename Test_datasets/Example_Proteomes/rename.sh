8f16260b6885473a5b578eeb92bde3a44ed280f6
mgt0007 <mgt0007@c20-login01.cm.cluster>
2021-03-31 18:15:24 -0500

    Removed tracking for Test_dataset directory

--- Test_datasets/Example_Proteomes/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for FASTA in *.renamed_headers.fasta
-do
-	NEW_NAME=`echo "${FASTA}" | sed 's/.renamed_headers//'`
-	printf "Old name was ${FASTA}, new name is ${NEW_NAME}\n"
-	mv ${FASTA} ${NEW_NAME}
-done
