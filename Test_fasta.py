0fd13ae5861e3eb6f87e25b06e0e5046c90c593d
Michael Tassia <mgt0007@auburn.edu>
2021-05-26 17:00:14 -0500

    Revert "Added Test_fasta.py to directory."
    This reverts commit af1e0236b8e677a2f86c3930629e07de84a83885.

--- Test_fasta.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from Bio import SeqIO
-import sys
-
-# $1 is the file to be tested; if it is a fasta file, print "TRUE"; if it is not a fasta file, print "FALSE"
-FASTA=sys.argv[1]
-FASTA=SeqIO.parse(FASTA,"fasta")
-print(any(FASTA))
