019bccba1cf1a0c2d2fa3fc6000561665dee7b5f
mtassia1 <mtassia1@login03.rockfish.cluster>
2022-12-16 10:11:52 -0500

    Updated Grab_models.sh for improved ease-of-use

--- Support_scripts/Grab_models.sh
+++ Support_scripts/Grab_models.sh
@@ -1,28 +1,92 @@
-#!/bin/bash
-# $1 input is list of pfam accessions (e.g., PF00065)
-# $2 input is Pfam-A.hmm
-# $3 input is Pfam-A.seed
+#!/usr/bin/env bash
 # Stockholm2Fasta.py has to be in the same directory as this script
 
-S2F=`echo "$0" | sed 's/Grab_models.sh$/Stockholm2Fasta.py/'`
-
-if [[ ! -s ${1} ]];then
-	echo "Input does cannot be found. Exiting..."
-	exit 0
+#If no arguments are passed to Grab_models.sh, print help output
+if [[ "$#" == 0 ]]; then
+	printf "\t-i [pfam_accession_list.txt]\tSpecify path to file containing a new-line delimited list of pfam accessions (e.g., PF01582.23)"
+	printf "\n\t-p [Pfam-A.hmm]\t\t\tSpecify path to uncompressed Pfam database"
+	printf "\n\t-s [Pfam-A.seed]\t\tSpecify path to uncompressed and indexed Pfam seed dataset\n"
+	exit 1
 fi
 
+#Program command variables & execution check
+S2F=`echo "$0" | sed 's/Grab_models.sh$/Stockholm2Fasta.py/'`
+HMMFETCH=`which hmmfetch 2>/dev/null`
+ESLAFETCH=`which esl-afetch 2>/dev/null`
+
+if ! [[ -x $HMMFETCH ]];then
+	EXECUTABLE_ERROR=`echo "TRUE"`
+	printf "ERROR: Cannot find hmmfetch executable in \$PATH\n"
+fi
+
+if ! [[ -x $ESLAFETCH ]];then
+	EXECUTABLE_ERROR=`echo "TRUE"`
+	printf "ERROR: Cannot find esl-afetch executable in \$PATH\n"
+fi
+
+if [[ ${EXECUTABLE_ERROR} == "TRUE" ]]; then #If any program cannot be executed, exit
+        printf "Exiting...\n"
+        exit 1
+fi
+
+#User-input variable definitions
+while getopts ":i:p:s:" opt; do
+	case $opt in
+		i) # Set path to user-specified Pfam accession list
+			ACCESSION_LIST=`echo "${OPTARG}"`
+			;;
+		p) # Set path to Pfam database
+			PFAM_DB_PATH=`echo "${OPTARG}"`
+			;;
+		s) # Set path to Pfam seed database
+			PFAM_SEEDS=`echo "${OPTARG}"`
+			;;
+		\?) # Error call if invalid flag provided
+			echo "Invalid option -${OPTARG}"
+			exit 1
+			;;
+		:) # Error call if flag reguires an argument and none provided
+			echo "Flag -$OPTARG requires an argument"
+			exit 1
+			;;
+	esac
+done
+
+#Input check
+ERROR=`echo "FALSE"`
+
+if [[ ! -s ${ACCESSION_LIST} ]];then
+	echo "List of Pfam accessions cannot be found."
+	ERROR=`echo "TRUE"`
+fi
+if [[ ! -s ${PFAM_DB_PATH} ]];then
+	echo "Pfam database cannot be found."
+	ERROR=`echo "TRUE"`
+fi
+if [[ ! -s ${PFAM_SEEDS} ]];then
+	echo "Pfam seed database cannot be found."
+	ERROR=`echo "TRUE"`
+fi
+if [[ ! -s ${PFAM_SEEDS}.ssi ]];then
+	echo "Pfam seed database is not indexed. Index using esl-afetch --index [Pfam-A.seeds]."
+	ERROR=`echo "TRUE"`
+fi
+if [[ ${ERROR} == "TRUE" ]]; then #If any errors above, exit.
+	printf "\nPlease check the ERROR reports above and try again.\nExiting...\n"
+	exit 1
+fi
+
+#Grab models and seeds
 while read PFAM_ACCESSION
 do
 	echo "...Obtaining ${PFAM_ACCESSION} profile HMM"
-	#wget "https://pfam.xfam.org/family/${PFAM_ACCESSION}/hmm"		
-	hmmfetch $2 ${PFAM_ACCESSION} > hmm
+	$HMMFETCH ${PFAM_DB_PATH} ${PFAM_ACCESSION} > hmm
 	NAME=`head hmm | grep 'NAME' | awk '{print $2}'`
 	SHORT_ACCESSION=`head hmm | grep 'ACC' | awk '{print $2}' | awk -F"." '{print $1}'`
 	mv hmm ${SHORT_ACCESSION}_${NAME}.hmm
 
 	echo "...Obtaining ${PFAM_ACCESSION} seed alignment"
-	#wget "https://pfam.xfam.org/family/${PFAM_ACCESSION}/alignment/seed"
-	esl-afetch $3 ${PFAM_ACCESSION} > seed
-	${S2F} seed ${SHORT_ACCESSION}_${NAME}.fasta
+	$ESLAFETCH $PFAM_SEEDS ${PFAM_ACCESSION} > seed
+	$S2F seed ${SHORT_ACCESSION}_${NAME}.fasta
 	rm seed
-done < $1
+done < ${ACCESSION_LIST}
